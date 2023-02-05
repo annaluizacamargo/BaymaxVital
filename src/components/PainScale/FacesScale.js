2246a09058bcfbd76725e2871b0a0e0ebd2a755f
Anna Luiza <luizafistarol@gmail.com>
2023-02-05 20:40:49 -0300

    feat(FacesScale): edit icons and colors

--- src/components/PainScale/FacesScale.js
+++ src/components/PainScale/FacesScale.js
@@ -2,89 +2,94 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import { styled } from '@mui/material/styles';
 import Rating from '@mui/material/Rating';
-import SentimentVerySatisfiedOutlinedIcon from '@mui/icons-material/SentimentVerySatisfiedOutlined';
-import SentimentVerySatisfiedIcon from '@mui/icons-material/SentimentVerySatisfied';
-import SentimentSatisfiedOutlinedIcon from '@mui/icons-material/SentimentSatisfiedOutlined';
-import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
-import SentimentSatisfiedIcon from '@mui/icons-material/SentimentSatisfied';
-import SentimentNeutralOutlinedIcon from '@mui/icons-material/SentimentNeutralOutlined';
-import SentimentNeutralIcon from '@mui/icons-material/SentimentNeutral';
-import SentimentDissatisfiedIcon from '@mui/icons-material/SentimentDissatisfied';
-import SentimentDissatisfiedOutlinedIcon from '@mui/icons-material/SentimentDissatisfiedOutlined';
-import MoodBadOutlinedIcon from '@mui/icons-material/MoodBadOutlined';
-import SentimentVeryDissatisfiedOutlinedIcon from '@mui/icons-material/SentimentVeryDissatisfiedOutlined';
+import {
+    SentimentVerySatisfiedOutlined,
+    SentimentVerySatisfied,
+    SentimentSatisfiedOutlined,
+    SentimentSatisfiedAlt,
+    SentimentSatisfied,
+    SentimentNeutralOutlined,
+    SentimentNeutral,
+    SentimentDissatisfied,
+    SentimentDissatisfiedOutlined,
+    MoodBadOutlined,
+    SentimentVeryDissatisfiedOutlined
+} from '@mui/icons-material';
 
 const StyledRating = styled(Rating)(({ theme }) => ({
-  '& .MuiRating-iconEmpty .MuiSvgIcon-root': {
-    color: theme.palette.action.disabled,
-  },
+    '& .MuiRating-iconEmpty .MuiSvgIcon-root': {
+        color: theme.palette.action.disabled,
+    },
+    '& .MuiRating-iconEmpty .MuiSvgIcon-root': {
+        color: 'var(--black)',
+    },
 }));
 
 const customIcons = {
     1: {
-        icon: <SentimentVerySatisfiedOutlinedIcon color="error" />,
-        label: 'Pain: 0',
-      },
-      2: {
-        icon: <SentimentVerySatisfiedIcon color="error" />,
-        label: 'Pain: 1',
-      },
-      3: {
-        icon: <SentimentSatisfiedOutlinedIcon color="warning" />,
-        label: 'Pain: 2',
-      },
-      4: {
-        icon: <SentimentSatisfiedAltIcon color="success" />,
-        label: 'Pain: 3',
-      },
-      5: {
-        icon: <SentimentSatisfiedIcon color="success" />,
-        label: 'Pain: 4',
-      },
-      6: {
-        icon: <SentimentNeutralOutlinedIcon color="error" />,
-        label: 'Pain: 5',
-      },
-      7: {
-        icon: <SentimentNeutralIcon color="error" />,
-        label: 'Pain: 6',
-      },
-      8: {
-        icon: <SentimentDissatisfiedIcon color="warning" />,
-        label: 'Pain: 7',
-      },
-      9: {
-        icon: <SentimentDissatisfiedOutlinedIcon color="success" />,
-        label: 'Pain: 8',
-      },
-      10: {
-        icon: <MoodBadOutlinedIcon color="success" />,
-        label: 'Pain: 9',
-      },
-      11: {
-        icon: <SentimentVeryDissatisfiedOutlinedIcon color="success" />,
-        label: 'Pain: 10',
-      },
+        icon: <SentimentVerySatisfiedOutlined color='success' />,
+        label: '0',
+    },
+    2: {
+        icon: <SentimentVerySatisfied color="success" />,
+        label: '1',
+    },
+    3: {
+        icon: <SentimentSatisfiedOutlined color="success" />,
+        label: '2',
+    },
+    4: {
+        icon: <SentimentSatisfiedAlt color="secondary" />,
+        label: '3',
+    },
+    5: {
+        icon: <SentimentSatisfied color="secondary" />,
+        label: '4',
+    },
+    6: {
+        icon: <SentimentNeutralOutlined color="primary" />,
+        label: '5',
+    },
+    7: {
+        icon: <SentimentNeutral color="primary" />,
+        label: '6',
+    },
+    8: {
+        icon: <SentimentDissatisfied color="warning" />,
+        label: '7',
+    },
+    9: {
+        icon: <SentimentDissatisfiedOutlined color="warning" />,
+        label: '8',
+    },
+    10: {
+        icon: <MoodBadOutlined color="error" />,
+        label: '9',
+    },
+    11: {
+        icon: <SentimentVeryDissatisfiedOutlined color="error" />,
+        label: '10',
+    },
 };
 
 function IconContainer(props) {
-  const { value, ...other } = props;
-  return <span {...other}>{customIcons[value].icon}</span>;
+    const { value, ...other } = props;
+    return <span {...other}>{customIcons[value].icon}</span>;
 }
 
 IconContainer.propTypes = {
-  value: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
 };
 
 export default function RadioGroupRating() {
-  return (
-    <StyledRating
-      name="highlight-selected-only"
-      defaultValue={1}
-      IconContainerComponent={IconContainer}
-      getLabelText={(value) => customIcons[value].label}
-      max={11}
-      highlightSelectedOnly
-    />
-  );
+    return (
+        <StyledRating
+            name="highlight-selected-only"
+            defaultValue={1}
+            IconContainerComponent={IconContainer}
+            getLabelText={(value) => customIcons[value].label}
+            max={11}
+            highlightSelectedOnly
+        />
+    );
 }
