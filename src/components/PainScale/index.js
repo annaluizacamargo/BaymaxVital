abc331557e1437916a3044e81d4b06e83817d722
Anna Luiza <luizafistarol@gmail.com>
2023-02-07 00:09:12 -0300

    feat(PainScale): h4 colors

--- src/components/PainScale/index.js
+++ src/components/PainScale/index.js
@@ -19,12 +19,12 @@ const PainScale = () => {
                 </div>
             </div>
             <div className='description'>
-                <h4>Sem Dor</h4>
-                <h4>Leve</h4>
-                <h4>Moderada</h4>
-                <h4>Forte</h4>
-                <h4>Muito Forte</h4>
-                <h4>Pior Possível</h4>
+                <h4 className='one' id='zero'>Sem Dor</h4>
+                <h4 className='two' id='one'>Leve</h4>
+                <h4 className='two' id='two'>Moderada</h4>
+                <h4 className='two' id='three'>Forte</h4>
+                <h4 className='two' id='four'>Muito Forte</h4>
+                <h4 className='two' id='five'>Pior Possível</h4>
             </div>
         </div>
     );
