9bd8193d1b8d0a6f666bcc43d838c20820b7af94
Anna Luiza <luizafistarol@gmail.com>
2023-02-08 00:32:10 -0300

    feat(VoiceRecording): add react-audio-voice-recorder

--- src/components/VoiceRecording/index.js
+++ src/components/VoiceRecording/index.js
@@ -2,17 +2,39 @@ import './style.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMicrophone } from '@fortawesome/free-solid-svg-icons'
 import { Button } from '@mui/material';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { AudioRecorder, useAudioRecorder } from 'react-audio-voice-recorder';
+
+
+const addAudioElement = (blob) => {
+    const url = URL.createObjectURL(blob);
+    const audio = document.createElement("audio");
+    audio.src = url;
+    audio.controls = true;
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <AudioRecorder onRecordingComplete={addAudioElement} />
+    </React.StrictMode>
+);
 
 const VoiceRecording = (props) => {
     const colorPrimary = `var(--${props.color}-primary)`;
     const colorSecondary = `var(--${props.color}-secondary)`;
-    console.log(props.color);
-    
+    const recorderControls = useAudioRecorder();
+
     return (
         <div className='voice-recording'>
-            <p className='transcription' style={{backgroundColor: colorPrimary}}>Aqui neste box será inserida a transcrição do áudio do paciente para fins de registro médico.</p>
+            <p className='transcription' style={{ backgroundColor: colorPrimary }}>Aqui neste box será inserida a transcrição do áudio do paciente para fins de registro médico.</p>
             <div className='buttons'>
-                <FontAwesomeIcon icon={faMicrophone} className='icon' style={{color: colorSecondary}}/>
+                <AudioRecorder
+                    className='recover'
+                    style={{color: colorSecondary}}
+                    onRecordingComplete={(blob) => addAudioElement(blob)}
+                    recorderControls={recorderControls}
+                />
                 <Button variant="btnConfirm">Confirmar</Button>
             </div>
         </div>
