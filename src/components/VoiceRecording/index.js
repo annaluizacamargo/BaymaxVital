1aacff3ea356c31bc8b451d8d7d9b998b3af923b
Anna Luiza <luizafistarol@gmail.com>
2023-02-07 23:52:53 -0300

    feat(VoiceRecording): edit style btns

--- src/components/VoiceRecording/index.js
+++ src/components/VoiceRecording/index.js
@@ -1,15 +1,19 @@
 import './style.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMicrophone } from '@fortawesome/free-solid-svg-icons'
+import { Button } from '@mui/material';
 
-const VoiceRecording = () => {
-    console.log('VoiceRecording');
+const VoiceRecording = (props) => {
+    const colorPrimary = `var(--${props.color}-primary)`;
+    const colorSecondary = `var(--${props.color}-secondary)`;
+    console.log(props.color);
+    
     return (
         <div className='voice-recording'>
-            <p className='transcription'>Aqui neste box será inserida a transcrição do áudio do paciente para fins de registro médico.</p>
+            <p className='transcription' style={{backgroundColor: colorPrimary}}>Aqui neste box será inserida a transcrição do áudio do paciente para fins de registro médico.</p>
             <div className='buttons'>
-                <FontAwesomeIcon icon={faMicrophone} className='icon' />
-                <button>CONFIRMAR</button>
+                <FontAwesomeIcon icon={faMicrophone} className='icon' style={{color: colorSecondary}}/>
+                <Button variant="btnConfirm">Confirmar</Button>
             </div>
         </div>
     );
