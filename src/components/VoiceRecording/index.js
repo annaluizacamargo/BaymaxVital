1ecc8952197a84a5dcc10219a0355adaedb3be3d
Anna Luiza <luizafistarol@gmail.com>
2023-02-07 22:39:30 -0300

    feat: add VoiceRecording

--- src/components/VoiceRecording/index.js
+++ src/components/VoiceRecording/index.js
@@ -1,8 +1,18 @@
 import './style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMicrophone } from '@fortawesome/free-solid-svg-icons'
 
 const VoiceRecording = () => {
     console.log('VoiceRecording');
-    //return ();
+    return (
+        <div className='voice-recording'>
+            <p className='transcription'>Aqui neste box será inserida a transcrição do áudio do paciente para fins de registro médico.</p>
+            <div className='buttons'>
+                <FontAwesomeIcon icon={faMicrophone} className='icon' />
+                <button>CONFIRMAR</button>
+            </div>
+        </div>
+    );
 };
 
 export default VoiceRecording;
