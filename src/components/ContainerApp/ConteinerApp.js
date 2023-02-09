2a4f6924b057235e93c558d866be757d4137b6cb
Anna Luiza <luizafistarol@gmail.com>
2023-02-09 01:12:48 -0300

    feat(ContainerApp): add return

--- src/components/ContainerApp/ConteinerApp.js
+++ src/components/ContainerApp/ConteinerApp.js
@@ -1,7 +1,26 @@
+import BoxBaymax from '../BoxBaymax';
+import PainScale from '../PainScale';
+import VoiceRecording from '../VoiceRecording';
 import './style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 
 const ContainerApp = () => {
     console.log('ContainerApp')
+    return (
+        <div className='container-app'>
+            <FontAwesomeIcon icon={faChevronLeft} className='btn-return' />
+            <div className='box'>
+                <BoxBaymax />
+                <PainScale />
+            </div>
+        </div>
+    )
 }
+
+//<BoxBaymax />
+//<VoiceRecording color='blue' />
+//<VoiceRecording color='orange' />
+//<PainScale />
 
 export default ContainerApp;
