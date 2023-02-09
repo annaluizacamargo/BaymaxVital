1cc11412077465abdecf7beac4d0c99f96935483
Anna Luiza <luizafistarol@gmail.com>
2023-02-08 23:43:49 -0300

    feat: add return BoxBaymax

--- src/components/BoxBaymax/index.js
+++ src/components/BoxBaymax/index.js
@@ -2,7 +2,16 @@ import './style.css';
 
 const BoxBaymax = () => {
     console.log('BoxBaymax');
-    //return ();
+    return (
+        <div className='box-baymax'>
+            <div className='img-baymax'>
+                <img src='./images/baymax-1.svg'></img>
+            </div>
+            <div className='box-text'>
+                <h3>Olá [nome usuário], eu sou Baymax, seu companheiro de cuidados pessoais de saúde</h3>
+            </div>
+        </div>
+    );
 };
 
 export default BoxBaymax;
