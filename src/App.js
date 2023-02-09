05744b38e8acff2613289c415dc7741136aa58a2
Anna Luiza <luizafistarol@gmail.com>
2023-02-08 23:58:06 -0300

    feat: add ContainerApp

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import PainScale from './components/PainScale';
 import VoiceRecording from './components/VoiceRecording';
 import BoxBaymax from './components/BoxBaymax';
+import ContainerApp from './components/ContainerApp/ConteinerApp';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
+      <ContainerApp />
       <BoxBaymax />
       <VoiceRecording color='blue' />
       <VoiceRecording color='orange' />
---
+++ src/components/ContainerApp/ConteinerApp.js
@@ -0,0 +1,7 @@
+import './style.css';
+
+const ContainerApp = () => {
+    console.log('ContainerApp')
+}
+
+export default ContainerApp;
