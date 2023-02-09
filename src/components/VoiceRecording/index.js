030f78ec868aa68b13e0ce2b7fa7b78f9cc17e7e
Anna Luiza <luizafistarol@gmail.com>
2023-02-08 22:53:20 -0300

    feat(VoiceRecording): add style audio-voice-recorder

--- src/components/VoiceRecording/index.js
+++ src/components/VoiceRecording/index.js
@@ -1,11 +1,7 @@
 import './style.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMicrophone } from '@fortawesome/free-solid-svg-icons'
 import { Button } from '@mui/material';
 import React from "react";
-import ReactDOM from "react-dom/client";
 import { AudioRecorder, useAudioRecorder } from 'react-audio-voice-recorder';
-
 
 const addAudioElement = (blob) => {
     const url = URL.createObjectURL(blob);
@@ -13,12 +9,6 @@ const addAudioElement = (blob) => {
     audio.src = url;
     audio.controls = true;
 };
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <AudioRecorder onRecordingComplete={addAudioElement} />
-    </React.StrictMode>
-);
 
 const VoiceRecording = (props) => {
     const colorPrimary = `var(--${props.color}-primary)`;
@@ -30,8 +20,7 @@ const VoiceRecording = (props) => {
             <p className='transcription' style={{ backgroundColor: colorPrimary }}>Aqui neste box será inserida a transcrição do áudio do paciente para fins de registro médico.</p>
             <div className='buttons'>
                 <AudioRecorder
-                    className='recover'
-                    style={{color: colorSecondary}}
+                    className='audio-recorder'
                     onRecordingComplete={(blob) => addAudioElement(blob)}
                     recorderControls={recorderControls}
                 />
