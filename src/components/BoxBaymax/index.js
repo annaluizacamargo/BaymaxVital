67adfadbd6cd7dc8304ddd90bc4317e3a0ffae4e
Anna Luiza <luizafistarol@gmail.com>
2023-02-09 21:17:46 -0300

    feat(BoxBaymax): add responsive style

--- src/components/BoxBaymax/index.js
+++ src/components/BoxBaymax/index.js
@@ -8,7 +8,7 @@ const BoxBaymax = () => {
                 <img src='./images/baymax-1.svg'></img>
             </div>
             <div className='box-text'>
-                <h3>Olá [nome usuário], eu sou Baymax, seu companheiro de cuidados pessoais de saúde</h3>
+                <h3>Olá [nome usuário], eu sou Baymax</h3>
             </div>
         </div>
     );
