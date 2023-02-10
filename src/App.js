e27fe6d232a907695504a0cf61b047658be33610
Anna Luiza <luizafistarol@gmail.com>
2023-02-10 15:36:17 -0300

    feat(App): add GoodScale

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import BadScale from './components/ContainerApp/BadScale';
+import GoodScale from './components/ContainerApp/GoodScale';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
-      <BadScale />
+      <GoodScale />
     </div>
   );
 }
