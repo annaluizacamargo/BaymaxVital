69911e34c6121db3f415cb52d3a5d72fae2e49df
Anna Luiza <luizafistarol@gmail.com>
2023-02-11 20:08:49 -0300

    feat(BadScale): add Router

--- src/components/ContainerApp/BadScale.js
+++ src/components/ContainerApp/BadScale.js
@@ -1,15 +1,16 @@
 import BoxBaymax from '../BoxBaymax';
-import PainScale from '../PainScale';
 import VoiceRecording from '../VoiceRecording';
 import './style.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from "react-router-dom";
 
 const BadScale = () => {
-    console.log('BadScale ')
+    const navigate = useNavigate();
+    
     return (
         <div className='container-app'>
-            <FontAwesomeIcon icon={faChevronLeft} className='btn-return' />
+            <FontAwesomeIcon icon={faChevronLeft} className='btn-return' onClick={() => navigate('/')} />
             <div className='box'>
                 <BoxBaymax
                     img='./images/baymax-bad.svg'
@@ -24,10 +25,5 @@ const BadScale = () => {
         </div>
     )
 }
-
-//<BoxBaymax />
-//<VoiceRecording color='blue' />
-//<VoiceRecording color='orange' />
-//<PainScale />
 
 export default BadScale;
