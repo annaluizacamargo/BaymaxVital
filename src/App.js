e43d9f5cb64c598408bdd113a748e89bd651e0a4
Anna Luiza <luizafistarol@gmail.com>
2023-02-11 19:37:15 -0300

    feat: add Routers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import BadScale from './components/ContainerApp/BadScale';
-import GoodScale from './components/ContainerApp/GoodScale';
 import PainScaleMain from './components/ContainerApp/PainScaleMain';
 
 function App() {
