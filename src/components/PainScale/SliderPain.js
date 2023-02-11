e37897ee9085124441bb6b432f2ba20d4f3399f6
Anna Luiza <luizafistarol@gmail.com>
2023-02-11 18:23:14 -0300

    refactor: SliderPain

--- src/components/PainScale/SliderPain.js
+++ src/components/PainScale/SliderPain.js
@@ -1,28 +1,18 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
-import { useState } from "react";
 
 export default function SliderPain(props) {
-  //const [colorSlider, setColorSlider] = useState('');
-  const colorSlider = props.colorSlider
+  const colorSlider = props.colorSlider;
+  const functionChangeValue = props.changeValue;
+  const painValue = props.value;
+
   const [width, setWidth] = React.useState(window.innerWidth);
   const breakpoint = 760;
-  const functionChangeValue = props.changeValue
-  const painValue = props.value
 
   React.useEffect(() => {
     window.addEventListener("resize", () => setWidth(window.innerWidth));
   }, []);
-
-  const OnChange = (event) => {
-    const valueSlider = event.target.value;    
-    console.log('usestate', painValue)
-    console.log('value', valueSlider)
-    functionChangeValue(event)
-  }
-
-
 
   return (
     <Box sx={{ width: '100%' }}>
