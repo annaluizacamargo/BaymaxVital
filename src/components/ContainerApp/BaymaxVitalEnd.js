d74036cf8cb4b9e2d6b801cce1b5bdcbf1ab7043
Anna Luiza <luizafistarol@gmail.com>
2023-02-12 19:41:41 -0300

    fix(BaymaxVitalEnd): edit return to style

--- src/components/ContainerApp/BaymaxVitalEnd.js
+++ src/components/ContainerApp/BaymaxVitalEnd.js
@@ -1,4 +1,3 @@
-import BoxBaymax from '../BoxBaymax';
 import './style-end.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
@@ -11,12 +10,14 @@ const BaymaxVitalEnd = () => {
     return (
         <div className='container-app'>
             <FontAwesomeIcon icon={faChevronLeft} className='btn-return' onClick={() => navigate('/')}/>
-            <div className='box'>
-                <BoxBaymax
-                    img='./images/baymax-flower.svg'
-                    text='Vou avisar o médico e enfermeiro, qualquer coisa é só me chamar para falar comigo de novo'
-                />
-                <Button variant="btnConfirm" onClick={() => navigate('/')}>Confirmar</Button>
+            <div className='box-baymax-end'>
+                <div className='img-baymax'>
+                    <img src='./images/baymax-flower.svg'></img>
+                </div>
+                <div className='box-text'>
+                    <h3>Vou avisar o médico e enfermeiro, qualquer coisa é só me chamar para falar comigo de novo</h3>
+                    <Button className='btn-end' variant="btnConfirm" onClick={() => navigate('/')}>Confirmar</Button>
+                </div>
             </div>
         </div>
     )
