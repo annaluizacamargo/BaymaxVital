48653e578171cf7718680523e713f36f0d49547c
Anna Luiza <luizafistarol@gmail.com>
2023-02-14 20:23:10 -0300

    feat(VoiceRecording): disabled button confirm

--- src/components/VoiceRecording/index.js
+++ src/components/VoiceRecording/index.js
@@ -1,25 +1,31 @@
 import './style.css';
 import { Button } from '@mui/material';
-import React from "react";
+import React, { useState } from "react";
 import { AudioRecorder, useAudioRecorder } from 'react-audio-voice-recorder';
 import { useNavigate } from "react-router-dom";
 
-const addAudioElement = (blob, namePatient) => {
-    const url = URL.createObjectURL(blob);
-    const audio = document.createElement("audio");
-    audio.src = url;
-    audio.controls = true;
-    const addBtn = document.querySelector('.audio');
-    addBtn.appendChild(audio);
-    //localStorage.setItem(namePatient, JSON.stringify(blob))
-};
 
 const VoiceRecording = (props) => {
+    const addAudioElement = (blob, namePatient) => {
+        const url = URL.createObjectURL(blob);
+        const audio = document.createElement("audio");
+        audio.src = url;
+        audio.controls = true;
+        const addBtn = document.querySelector('.audio');
+        addBtn.appendChild(audio);
+        setStatusBtn(false)
+        //localStorage.setItem(namePatient, JSON.stringify(blob))
+    };
+
+    const [statusBtn, setStatusBtn] = useState(true)
+
+
     const navigate = useNavigate();
     const colorPrimary = `var(--${props.color}-primary)`;
     const colorSecondary = `var(--${props.color}-secondary)`;
     const namePatient = props.namePatient
     const recorderControls = useAudioRecorder();
+
 
     return (
         <div className='voice-recording'>
@@ -40,6 +46,7 @@ const VoiceRecording = (props) => {
                     />
                 </div>
                 <Button
+                    disabled={statusBtn}
                     variant='btnConfirm'
                     id='btnConfirm'
                     onClick={() => {
