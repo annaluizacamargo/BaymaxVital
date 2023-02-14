4d28187dccc87cb8bfd7a6f31ad1125e4d68a848
Anna Luiza <luizafistarol@gmail.com>
2023-02-14 12:00:54 -0300

    feat(VoiceRecording): color style

--- src/components/VoiceRecording/index.js
+++ src/components/VoiceRecording/index.js
@@ -17,25 +17,36 @@ const addAudioElement = (blob, namePatient) => {
 const VoiceRecording = (props) => {
     const navigate = useNavigate();
     const colorPrimary = `var(--${props.color}-primary)`;
+    const colorSecondary = `var(--${props.color}-secondary)`;
     const namePatient = props.namePatient
     const recorderControls = useAudioRecorder();
 
     return (
         <div className='voice-recording'>
+            <div className='audio' style={{ backgroundColor: colorPrimary }}>
+            </div>
             <div className='buttons'>
-                <AudioRecorder
-                    className='audio-recorder'
-                    onRecordingComplete={(blob) => addAudioElement(blob, namePatient)}
-                    recorderControls={recorderControls}
-                    style={{ backgroundColor: colorPrimary }}
-                />
-                <div className='audio' style={{ backgroundColor: colorPrimary }}>
+                <div className='recorder'
+                    style={{
+                        backgroundColor: colorPrimary,
+                        border: `${colorSecondary} 2px outset`,
+                        borderRadius: '5px'
+                    }}>
+                    <AudioRecorder
+                        className='audio-recorder'
+                        onRecordingComplete={(blob) => addAudioElement(blob, namePatient)}
+                        recorderControls={recorderControls}
+                        style={{ backgroundColor: colorPrimary }}
+                    />
                 </div>
-                <Button variant="btnConfirm" onClick={() => {
-                    document.getElementsByTagName('audio').length > 0
-                        ? navigate('/baymax-vital-end')
-                        : alert('Queremos melhorar cada vez mais, por favor clique no botão do microfone e nos diga o que podemos fazer para te deixar melhor 🥰')
-                }}>Confirmar</Button>
+                <Button
+                    variant='btnConfirm'
+                    id='btnConfirm'
+                    onClick={() => {
+                        document.getElementsByTagName('audio').length > 0
+                            ? navigate('/baymax-vital-end')
+                            : alert('Queremos melhorar cada vez mais, por favor clique no botão do microfone e nos diga o que podemos fazer para te deixar melhor 🥰')
+                    }}>Confirmar</Button>
             </div>
         </div>
     );
