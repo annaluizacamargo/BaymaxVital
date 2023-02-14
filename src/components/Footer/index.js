a93009750d9b4c8058dad9a954cb6cd393f519a5
Anna Luiza <luizafistarol@gmail.com>
2023-02-14 09:25:57 -0300

    feat(Footer): add Footer elements

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,32 @@
 import './style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Footer = () => {
     console.log('Footer');
-    //return (
-//
-    //)
+    return (
+        <footer className='footer'>
+            <div className='credits'>
+                <p>© Desenvolvido por Anna Luiza - 2023</p>
+            </div>
+            <ul className='contact'>
+                <li>
+                    <a href='https://www.linkedin.com/in/anna-luiza-camargo-fistarol/'>
+                        <FontAwesomeIcon icon={faLinkedinIn} />
+                    </a>
+                </li>
+                <li>
+                    <a href='https://github.com/annaluizacamargo'>
+                        <FontAwesomeIcon icon={faGithubAlt} />
+                    </a>
+                </li>
+                <li>
+                    <a href='mailto:luizafistarol@gmail.com'>
+                        <FontAwesomeIcon icon={faEnvelope} />
+                    </a>
+                </li>
+            </ul>
+        </footer>
+    )
 }
 
 export default Footer;
