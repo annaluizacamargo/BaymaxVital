852201e6a3f100ce2c406e70de1f693b603b7401
Anna Luiza <luizafistarol@gmail.com>
2023-02-15 03:15:15 -0300

    refactor(VoiceRecording): components

--- src/components/VoiceRecording/components/index.js
+++ src/components/VoiceRecording/components/index.js
@@ -4,40 +4,37 @@ import React, { useState } from "react";
 import { AudioRecorder, useAudioRecorder } from 'react-audio-voice-recorder';
 import { useNavigate } from "react-router-dom";
 
-
 const VoiceRecording = (props) => {
-    const addAudioElement = (blob, namePatient) => {
-        const url = URL.createObjectURL(blob);
-        const audio = document.createElement("audio");
-        audio.src = url;
-        audio.controls = true;
-        const addBtn = document.querySelector('.audio');
-        addBtn.appendChild(audio);
-        setStatusBtn(false)
-        //localStorage.setItem(namePatient, JSON.stringify(blob))
-    };
-
-    const [statusBtn, setStatusBtn] = useState(true)
-
-
-    const navigate = useNavigate();
+    const [statusBtn, setStatusBtn] = useState(true);
+    const recorderControls = useAudioRecorder();
     const colorPrimary = `var(--${props.color}-primary)`;
     const colorSecondary = `var(--${props.color}-secondary)`;
-    const namePatient = props.namePatient
-    const recorderControls = useAudioRecorder();
+    const namePatient = props.namePatient;
+    const navigate = useNavigate();
 
+    const addAudioElement = (blob) => {
+        const url = URL.createObjectURL(blob);
+        const audio = document.createElement("audio");
+        const addBtn = document.querySelector('.audio');
+        audio.src = url;
+        audio.controls = true;
+        addBtn.appendChild(audio);
+        setStatusBtn(false);
+    };
 
     return (
         <div className='voice-recording'>
-            <div className='audio' style={{ backgroundColor: colorPrimary }}>
-            </div>
+            <div className='audio' style={{ backgroundColor: colorPrimary }} />
+
             <div className='buttons'>
-                <div className='recorder'
+                <div
+                    className='recorder'
                     style={{
                         backgroundColor: colorPrimary,
                         border: `${colorSecondary} 2px outset`,
                         borderRadius: '5px'
                     }}>
+
                     <AudioRecorder
                         className='audio-recorder'
                         onRecordingComplete={(blob) => addAudioElement(blob, namePatient)}
@@ -45,15 +42,13 @@ const VoiceRecording = (props) => {
                         style={{ backgroundColor: colorPrimary }}
                     />
                 </div>
+
                 <Button
                     disabled={statusBtn}
                     variant='btnConfirm'
                     id='btnConfirm'
-                    onClick={() => {
-                        document.getElementsByTagName('audio').length > 0
-                            ? navigate('/baymax-vital-end')
-                            : alert('Queremos melhorar cada vez mais, por favor clique no botão do microfone e nos diga o que podemos fazer para te deixar melhor 🥰')
-                    }}>Confirmar</Button>
+                    //</div>onClick={() => {document.getElementsByTagName('audio').length > 0 ? navigate('/baymax-vital-end') : alert('Queremos melhorar cada vez mais, por favor clique no botão do microfone e nos diga o que podemos fazer para te deixar melhor 🥰')}}
+                >Confirmar</Button>
             </div>
         </div>
     );
